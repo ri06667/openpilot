ea21c55e42c40147704668c504601aa55f63806a
ri06667 <81943916+ri06667@users.noreply.github.com>
2026-08-23 20:36:10 -0600

    Update updated.py

--- system/updated/updated.py
+++ system/updated/updated.py
@@ -31,11 +31,11 @@ FINALIZED = os.path.join(STAGING_ROOT, "finalized")
 OVERLAY_INIT = Path(os.path.join(BASEDIR, ".overlay_init"))
 
 # do not allow to engage after this many hours onroad and this many routes
-HOURS_NO_CONNECTIVITY_MAX = 27
-ROUTES_NO_CONNECTIVITY_MAX = 84
+HOURS_NO_CONNECTIVITY_MAX = 9999
+ROUTES_NO_CONNECTIVITY_MAX = 9999
 # send an offroad prompt after this many hours onroad and this many routes
-HOURS_NO_CONNECTIVITY_PROMPT = 23
-ROUTES_NO_CONNECTIVITY_PROMPT = 80
+HOURS_NO_CONNECTIVITY_PROMPT = 9999
+ROUTES_NO_CONNECTIVITY_PROMPT = 9999
 
 
 class UserRequest:
